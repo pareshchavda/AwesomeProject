341fd0a39e243e3302c31590991f35c6b9a5f787
Paresh Chavda <paresh.chavda1432@gmail.com>
2024-04-29 16:01:41 +0530

    expo

--- App.js
+++ App.js
@@ -1,11 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from "expo-camera";
+import { useState, useEffect } from "react";
+import { Button, StyleSheet, Text, View, Platform } from "react-native";
+import BarcodeMask from "react-native-barcode-mask";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+
+  let x = 5;
+  let y = 10;
+  let z = 25;
+  x = y;
+  x = y = z;
+  console.log("X", x);
+  console.log("y", y);
+  console.log("z", z);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    console.log("Barcode data:", data);
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (!hasPermission) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to access the camera
+        </Text>
+        <Button
+          onPress={() => {
+            Camera.requestCameraPermissionsAsync().then(({ status }) => {
+              setHasPermission(status === "granted");
+            });
+          }}
+          title="Grant Permission"
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera
+        style={styles.camera}
+        type={Camera.Constants.Type.back}
+        onBarCodeScanned={handleBarCodeScanned}
+      >
+        <BarcodeMask edgeColor="#F2F2F2" showAnimatedLine />
+      </Camera>
     </View>
   );
 }
@@ -13,8 +64,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+  },
+  camera: {
+    flex: 1,
   },
 });
